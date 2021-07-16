ab1572ceb84d75f731b1b5d7d38e8e7061577758
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-07-16 18:17:22 +0300

    Create ex_2.py

---
+++ ex_2.py
@@ -0,0 +1,6 @@
+my_name = 'Marina'
+print(f'Length of my name is {len(my_name)} characters')
+print(f'My name into upper case: {my_name.upper()}')
+print(f'My name into lower case: {my_name.lower()}')
+print(f'The first symbol in my name is {my_name[:1]}')
+print(f'Symbol "a" appears in my name {my_name.count("a")} times')
